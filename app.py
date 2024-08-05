400b3f42ae6282dfae1a9bf5ce3889694436ec8f
ADUOIRDS <88500591+64070066-Kenny@users.noreply.github.com>
2024-08-05 21:38:59 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,24 +1,21 @@
 import os
 from flask import Flask, render_template
 
-app = Flask(__name__,template_folder="")
-
+app = Flask(__name__, template_folder=".")
 
 @app.route('/')
 def home():
-   
-   env_var_colour = os.environ['APP_COLOR']
-   
-   return render_template("index.html", color= env_var_colour)
-    
+    # Use 'red' as the default color if APP_COLOR is not set
+    env_var_colour = os.getenv('APP_COLOR', 'green')
+    return render_template("index.html", color=env_var_colour)
 
 @app.route('/<string:name>')
 def template(name):
     return render_template("index.html", color=name)
-    
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port="8081")
+    app.run(host="0.0.0.0", port=8081)
+
 
 
     
